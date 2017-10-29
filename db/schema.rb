dc3c7f0666c6b40403348d44e8ce501ced4200ee
ben2d2 <ben2d2@gmail.com>
2017-10-29 11:49:08 -0500

    Add GamePlayers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029164600) do
+ActiveRecord::Schema.define(version: 20171029164824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "game_players", force: :cascade do |t|
+    t.integer  "game_id"
+    t.integer  "player_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at"
