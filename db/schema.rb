f1bf9158ac2a48f34813e826263d50fad8f8b13e
ben2d2 <ben2d2@gmail.com>
2017-10-29 16:36:37 -0500

    Add :number to Rounds table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029212346) do
+ActiveRecord::Schema.define(version: 20171029213536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171029212346) do
     t.integer  "total"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "number"
   end
 
 end
