80b77f8437a0fc751d38aee943f2d4acb3c44444
ben2d2 <ben2d2@gmail.com>
2017-10-28 21:33:06 -0500

    Some scoring clean up

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,24 +1,25 @@
 class Round
   PHARCLE = [1, 2, 3, 4, 5, 6]
+  THREE_PAIRS = [2, 2, 2]
 
   def turn
     grouped_by_value = roll(6)
-    turn_one = score_and_such(grouped_by_value, 6)
+    turn_one = score_for(grouped_by_value, 6)
     hash = { "1" => turn_one }
-    if !turn_one[:score].zero?
-      dice_count = turn_one[:remaining_dice_count]
+    dice_count = turn_one[:remaining_dice_count]
+    if !turn_one[:score].zero? && !dice_count.zero?
       grouped_by_value = roll(dice_count)
-      turn_two = score_and_such(grouped_by_value, dice_count)
+      turn_two = score_for(grouped_by_value, dice_count)
       hash.merge!("2" => turn_two)
-      if !turn_two[:score].zero?
-        dice_count = turn_two[:remaining_dice_count]
+      dice_count = turn_two[:remaining_dice_count]
+      if !turn_two[:score].zero? && !dice_count.zero?
         grouped_by_value = roll(dice_count)
-        turn_three = score_and_such(grouped_by_value, dice_count)
+        turn_three = score_for(grouped_by_value, dice_count)
         hash.merge!("3" => turn_three)
-        if !turn_three[:score].zero?
-          dice_count = turn_three[:remaining_dice_count]
+        dice_count = turn_three[:remaining_dice_count]
+        if !turn_three[:score].zero? && !dice_count.zero?
           grouped_by_value = roll(dice_count)
-          turn_four = score_and_such(grouped_by_value, dice_count)
+          turn_four = score_for(grouped_by_value, dice_count)
           hash.merge!("4" => turn_four)
         end
       end
@@ -30,11 +31,11 @@ class Round
     DiceSet.new(dice_count).collection
   end
 
-  def score_and_such(grouped_by_value, dice_count)
+  def score_for(grouped_by_value, dice_count)
     score = 0
-    pairs = []
-    values = grouped_by_value.map { |k, v| v.map(&:value) }.flatten
+    values = values(grouped_by_value)
     return pharcle if values.sort == PHARCLE
+    return three_pairs if has_three_pairs?(grouped_by_value)
     grouped_by_value.each do |k, v|
       count = v.count
       if count >= 3
@@ -53,16 +54,12 @@ class Round
         else
           score += (100 * count)
         end
-      elsif count == 2
-        pairs << v
       else
         new_score = (mapping[k].to_i * count)
         score += new_score
         dice_count -= count unless new_score.zero?
       end
     end
-
-    return three_pairs(values) if pairs.size == 3
 
     {
       score: score,
@@ -81,7 +78,11 @@ class Round
   end
 
   def values(grouped_by_value)
-    grouped_by_value.map { |k, v| v.map { |dice| dice.value } }.flatten
+    grouped_by_value.map { |k, v| v.map(&:value) }.flatten
+  end
+
+  def has_three_pairs?(grouped_by_value)
+    grouped_by_value.map { |k, v| v.count } == THREE_PAIRS
   end
 
   def three_pairs(values)
