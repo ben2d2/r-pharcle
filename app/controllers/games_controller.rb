fd4352ede3996563b26b460683a4b109f2e0a964
ben2d2 <ben2d2@gmail.com>
2017-10-30 21:33:33 -0500

    Add ScoreboardPresenter

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,46 +12,6 @@ class GamesController < ApplicationController
     @player = Player.new
     @game_player = GamePlayer.new
     @game = Game.find(params[:id])
-    determine_round_number
-    @scoreboard = calculate_scores
-    @round = Round.new
-  end
-
-  private
-  def calculate_scores
-    players = @game.players.order('game_players.player_number')
-    header = players.pluck(:username).unshift("Round")
-    array = @game.rounds.group_by(&:number).sort_by { |k, v| v }.map do |k, v|
-      scores = [k]
-      players.map do |player|
-        v.each do |round|
-          scores << round.total if round.player_id == player.id
-        end
-      end
-      scores
-    end.unshift(header)
-    footer = ["Total"]
-    players.each do |player|
-      @game.rounds.group_by(&:player_id).each do |player_id, rounds|
-        footer << rounds.map(&:total).reduce(0, :+) if player_id == player.id
-      end
-    end
-    array << footer
-    array
-  end
-
-  def determine_round_number
-    if @game.rounds.blank?
-      @round_number = 1
-    else
-      grouped_by_number = @game.rounds.group_by { |r| r.number }.sort_by { |k, v| k }
-      grouped_by_number.each do |k, v|
-        if v.to_a.count < @game.players.count
-          @round_number = k
-        else
-          @round_number = k.to_i + 1
-        end
-      end
-    end
+    @scoreboard = ScoreboardPresenter.new(@game).to_h
   end
 end
---
+++ app/models/scoreboard_presenter.rb
@@ -0,0 +1,82 @@
+class ScoreboardPresenter
+  attr_reader :game, :round_number
+
+  def initialize(game)
+    @game         = game
+    @round_number = determine_round_number
+  end
+
+  def to_h
+    {
+      header_row: players.pluck(:username).unshift("Round"),
+      totals_row: totals_row,
+      data_rows: data_rows,
+      next_round_row: next_round_row,
+      add_next_row: add_next_row?
+    }
+  end
+
+  private
+  def data_rows
+    grouped_and_sorted_rounds.map do |k, rounds|
+      row = [k]
+      by_player = rounds_by_player(rounds)
+      players.each do |player|
+        if player_round = by_player[player.id]
+          row << player_round.first.total
+        else
+          row << form_data_for(player.id)
+        end
+      end
+      row
+    end
+  end
+
+  def totals_row
+    players.map do |player|
+      rounds = rounds_by_player.fetch(player.id, [])
+      rounds.map(&:total).reduce(0, :+)
+    end.unshift("Total")
+  end
+
+  def next_round_row
+    players.map { |player| form_data_for(player.id) }.unshift(round_number)
+  end
+
+  def form_data_for(player_id)
+    { form_data: { game_id: game.id, player_id: player_id, number: round_number } }
+  end
+
+  def players
+    @players ||= game.players.order('game_players.player_number')
+  end
+
+  def game_rounds
+    @game_rounds ||= game.rounds
+  end
+
+  def grouped_and_sorted_rounds
+    @grouped_and_sorted_rounds ||= game_rounds.group_by(&:number).sort_by { |k, v| k }
+  end
+
+  def rounds_by_player(rounds=game_rounds)
+    rounds.group_by(&:player_id)
+  end
+
+  def add_next_row?
+    game_rounds.where(number: round_number).any?
+  end
+
+  def determine_round_number
+    return 1 if game_rounds.blank?
+    number = 0
+    grouped_and_sorted_rounds.each do |k, v|
+      if v.to_a.count < players.count
+        number = k
+      else
+        number = k.to_i + 1
+      end
+    end
+    number
+  end
+end
