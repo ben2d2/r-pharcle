4548f3086efa14451daa25b61a9441705fb1b00f
ben2d2 <ben2d2@gmail.com>
2017-11-05 16:32:16 -0600

    Clean up some views and exposed data

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,11 @@
 class GamesController < ApplicationController
-  expose(:games)
+  expose(:games) { Game.all }
   expose(:game)
   expose(:player) { Player.new }
   expose(:game_player) { GamePlayer.new }
   expose(:scoreboard) { ScoreboardPresenter.new(game) }
   expose(:flash_type) { params[:flash_type] }
+
   def index; end
 
   def create
